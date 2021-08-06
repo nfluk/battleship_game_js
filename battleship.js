c35db52b35ad7a0c43ccc53d27eaed78f09aa1d2
deroj <nickylukic@gmail.com>
2021-08-06 13:12:47 +0200

    moved down "var stats"

--- battleship.js
+++ battleship.js
@@ -8,26 +8,24 @@ var guesses = 0;
 
 var isSunk = false;
 
-var stats = "You took " + guesses + " guesses to sink the battleship. Your shooting accuracy was " + (3/guesses)".";
-
 while (isSunk == false) {
-guess = prompt("Ready, aim, fire! (enter a number between 0-6):");
-if (guess < 0 || > 6 ) {
-    alert("Please enter a number between 0-6");
-} 
-else {
-guesses = guesses + 1;
+    guess = prompt("Ready, aim, fire! (enter a number between 0-6):");
+    if (guess < 0 || > 6 ) {
+        alert("Please enter a number between 0-6");
+    } else {
+        guesses = guesses + 1;
 
-if (guess == location1 || guess == location2 || guess == location3) {
-    hits = hits + 1;
-}
-}
-if (hits == 3) {
-    isSunk = true;
-    alert("Wow! You nailed it, captain! You have sunk the battleship!");
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+
+            if (hits == 3) {
+             isSunk = true;
+                alert("Wow! You nailed it, captain! You have sunk the battleship!");
+            }
+        }
+    }
 }
 
-
-}
+var stats = "You took " + guesses + " guesses to sink the battleship. Your shooting accuracy was " + (3/guesses)".";
 
 alert(stats);
