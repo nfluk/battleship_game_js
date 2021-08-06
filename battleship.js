6fba860fdb1afad457c1cc51f827382cef480271
deroj <nickylukic@gmail.com>
2021-08-06 13:29:34 +0200

    added "%" to "stats" variable

--- battleship.js
+++ battleship.js
@@ -25,5 +25,5 @@ while (isSunk == false) {
 	}
 }
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
-            "which means your shooting accuracy was " + (3/guesses);
+            "which means your shooting accuracy was " + (3/guesses * 100)"%";
 alert(stats);
