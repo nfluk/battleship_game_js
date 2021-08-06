44975b58903f50efc28aacad175f9fc092a4d898
deroj <nickylukic@gmail.com>
2021-08-06 13:06:00 +0200

    added first logic

--- battleship.js
+++ battleship.js
@@ -7,3 +7,27 @@ var hits = 0;
 var guesses = 0;
 
 var isSunk = false;
+
+var stats = "You took " + guesses + " guesses to sink the battleship. Your shooting accuracy was " + (3/guesses)".";
+
+while (isSunk == false) {
+guess = prompt("Ready, aim, fire! (enter a number between 0-6):");
+if (guess < 0 || > 6 ) {
+    alert("Please enter a number between 0-6");
+} 
+else {
+guesses = guesses + 1;
+
+if (guess == location1 || guess == location2 || guess == location3) {
+    hits = hits + 1;
+}
+}
+if (hits == 3) {
+    isSunk = true;
+    alert("Wow! You nailed it, captain! You have sunk the battleship!");
+}
+
+
+}
+
+alert(stats);
